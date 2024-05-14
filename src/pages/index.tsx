2f5f4a36a21c4e3954615dfe5bd566063f809a37
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-05-14 04:32:44 -0300

    Update: index.tsx -> h1 font size v5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col justify-center bg-fundo-site bg-cover ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center bg-fundo-site bg-cover ${inter.className}`}
     >
       <Header />
       <h1 className={`sm:text-2xl lg:text-4xl font-caveat text-red-600`}>Pedra Preta Surf Master 2024</h1>
