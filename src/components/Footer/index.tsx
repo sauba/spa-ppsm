3b91c0fd68f6fc0374e27b2e90905361c7fcbafc
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-05-15 16:05:33 -0300

    Update: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -20,9 +20,9 @@ export default function Footer() {
       <div className={`flex-col justify-center items-center`}>
         <a href="https://wa.me/5581985259168" referrerPolicy="no-referrer" target="_blank" className={`px-4 py-2 mx-auto flex justify-center items-center gap-3 rounded-3xl font-caveat`}>
           <WhatsappLogo size={48} weight="thin" color={`lime`} className={`hover:cursor-pointer`} />
-          <p className={`text-center text-xl lg:text-5xl`}>Informações</p>
+          <p className={`text-center text-2xl lg:text-5xl`}>Informações</p>
         </a>
-        <p className={`text-center text-xl lg:text-5xl font-caveat`}>Acompanhe as novidades no nosso instagram</p>
+        <p className={`text-center text-2xl lg:text-5xl font-caveat`}>Acompanhe as novidades no nosso instagram</p>
         <a href="https://www.instagram.com/parafajsp/" referrerPolicy="no-referrer" target="_blank">
           <InstagramLogo size={68} weight="thin" color="red" className={`hover:cursor-pointer animate-wiggle mx-auto py-2`} />
         </a>
