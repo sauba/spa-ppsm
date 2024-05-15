26746d213ce82a0320b49ffbf438dd60fa59fce4
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-05-15 01:59:06 -0300

    Update: Premiacao - inscrições size modified v4

--- src/components/Premiacao/index.tsx
+++ src/components/Premiacao/index.tsx
@@ -20,17 +20,17 @@ export default function Premiacao() {
         Inicio no sábado 13 de julho as 10:00hs na Praia de Xareu - Pico Pedra Preta.
       </p>
       <p className="text-center xl:text-2xl tracking-wider lg:pb-12">
-        Cabo de Sto Agostinho - Pernambuco
+        Cabo de Sto Agostinho - Pernambuco.
       </p>
       <div className="w-full grid grid-cols-2 justify-center items-center">
         <div className="lg:w-12/12">
           <span className="lg:text-3xl">Premiação Por Categoria:</span>
         </div>
         <div className="lg:w-11/12 flex flex-col">
-          <span className="text-xl lg:text-2xl lg:py-2">1º Lugar: Troféu, Bloco e Tatuagem no valor de R$500,00.</span>
-          <span className="text-xl lg:text-2xl lg:py-2">2º Lugar: Medalha e Kits.</span>
-          <span className="text-xl lg:text-2xl lg:py-2">3º Lugar: Medalha e Kits.</span>
-          <span className="text-xl lg:text-2xl lg:py-2">4º Lugar: Medalha e Kits.</span>
+          <span className="text-sm lg:text-2xl lg:py-2">1º Lugar: Troféu, Bloco e Tatuagem no valor de R$500,00.</span>
+          <span className="text-sm lg:text-2xl lg:py-2">2º Lugar: Medalha e Kits.</span>
+          <span className="text-sm lg:text-2xl lg:py-2">3º Lugar: Medalha e Kits.</span>
+          <span className="text-sm lg:text-2xl lg:py-2">4º Lugar: Medalha e Kits.</span>
         </div>
       </div>
     </div>
