c3d7afc445c22761182fd98293477807d0657de6
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-05-15 15:53:41 -0300

    Update: Agradecimentos update

--- src/components/Agradecimentos/index.tsx
+++ src/components/Agradecimentos/index.tsx
@@ -16,19 +16,19 @@ export default function Agradecimentos() {
       `} id="agradecimentos">
       <p className={`text-center text-5xl animate-skew-stop tracking-wider font-caveat`}>Agradecimentos</p>
 
-      <p className={`text-center lg:pt-8 text-sm lg:text-xl tracking-widest py-1`}>
+      <p className={`text-center lg:pt-8 text-sm lg:text-xl tracking-widest py-2`}>
         Agradecimentos especiais a <span className="xl:text-2xl font-bold">Jisk</span>, através de seu representante em Pernambuco, Rógerio, a <span className="xl:text-2xl font-bold">Fábrica de Óculos</span> da marca <span className="xl:text-2xl font-bold">Maldivas Eyewear</span>, a <span className="xl:text-2xl font-bold">Irado Tattoo</span>.
       </p>
 
-      <p className={`text-center text-sm lg:text-xl tracking-widest py-1`}>
+      <p className={`text-center text-sm lg:text-xl tracking-widest py-2`}>
         Aos apoiadores <span className="font-bold">LD Artes Gráficas</span>, as barracas mais alucinantes de praia da <span className="xl:text-2xl font-bold">Inside Tendas de Sol</span>, a loja mais surf do litoral cabense <span className="xl:text-2xl font-bold">Xareu Acessórios</span> e do nosso <span className="font-bold">Fotógrafo Oficial Marcos Paulo da MP Vídeos e Photos</span>.
       </p>
 
-      <p className={`text-center text-sm lg:text-xl tracking-widest py-1`}>
+      <p className={`text-center text-sm lg:text-xl tracking-widest py-2`}>
         Ao nosso querido amigo e um dos maiores incentivadores dos esportes <span className="font-bold">João Ivson</span>. a <span className="font-bold">Associação de Surf do Cabo de Santo Agostinho</span>, através do presidente Marcos Moraes.
       </p>
 
-      <p className={`text-center text-sm lg:text-xl tracking-widest py-1`}>
+      <p className={`text-center text-sm lg:text-xl tracking-widest py-2`}>
         E uma menção especial ao nosso Prefeito <span className="xl:text-2xl font-bold">Keko do Armazém</span> pelo incentivo aos esportes, que vem fazendo com que os atletas de todas as modalidades do municipio se dediquem e saibam que o esporte <span className="xl:text-2xl font-bold">Salva Vidas</span>.
       </p>
 
