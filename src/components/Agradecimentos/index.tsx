16552eccc1d45e9bf04c50c728db9ddf485a9a64
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-05-14 21:47:01 -0300

    Update: Agradecimentos Component lg:text-xl v2

--- src/components/Agradecimentos/index.tsx
+++ src/components/Agradecimentos/index.tsx
@@ -18,11 +18,11 @@ export default function Agradecimentos() {
       `} id="agradecimentos">
       <p className={`text-center xl:text-5xl animate-skew-stop tracking-wider font-caveat`}>Agradecimentos</p>
       <p className={`text-center lg:pt-8 lg:text-xl tracking-widest leading-loose py-1`}>
-        Agradecimentos especiais a <span className="xl:text-3xl font-bold">Jisk</span>, através de seu representante aqui em Pernambuco Rógerio, a <span className="xl:text-3xl font-bold">Fábrica de Óculos</span> da marca <span className="xl:text-3xl font-bold">Maldivas Eyewear</span>, dos meus amigos irmãos Lisandro e Elisio, a <span className="xl:text-3xl font-bold">Irado Tattoo</span> do meu parceiro Everton.
+        Agradecimentos especiais a <span className="xl:text-2xl font-bold">Jisk</span>, através de seu representante aqui em Pernambuco Rógerio, a <span className="xl:text-2xl font-bold">Fábrica de Óculos</span> da marca <span className="xl:text-2xl font-bold">Maldivas Eyewear</span>, dos meus amigos irmãos Lisandro e Elisio, a <span className="xl:text-2xl font-bold">Irado Tattoo</span> do meu parceiro Everton.
       </p>
 
       <p className={`text-center lg:text-xl tracking-widest leading-loose py-1`}>
-        Aos apoiadores <span className="font-bold">LD Artes Gráficas</span> do longboarder Zé Luis, as barracas mais alucinantes de praia da <span className="xl:text-3xl font-bold">Inside Tendas de Sol</span> do nosso amigo Kal, a loja mais surf do litoral cabense <span className="xl:text-3xl font-bold">Xareu Acessórios</span> do nosso surfista local Rafael e do nosso <span className="font-bold">Fotógrafo Oficial Marcos Paulo da MP Vídeos e Photos</span>.
+        Aos apoiadores <span className="font-bold">LD Artes Gráficas</span> do longboarder Zé Luis, as barracas mais alucinantes de praia da <span className="xl:text-2xl font-bold">Inside Tendas de Sol</span> do nosso amigo Kal, a loja mais surf do litoral cabense <span className="xl:text-2xl font-bold">Xareu Acessórios</span> do nosso surfista local Rafael e do nosso <span className="font-bold">Fotógrafo Oficial Marcos Paulo da MP Vídeos e Photos</span>.
       </p>
 
       <p className={`text-center lg:text-xl tracking-widest leading-loose py-1`}>
@@ -30,7 +30,7 @@ export default function Agradecimentos() {
       </p>
 
       <p className={`text-center lg:text-xl tracking-widest leading-loose py-1`}>
-        E uma menção especial ao nosso Prefeito <span className="xl:text-3xl font-bold">Keko do Armazém</span> pelo incentivo aos esportes, que vem fazendo com que os atletas de todas as modalidades do municipio se dediquem e saibam que o esporte <span className="xl:text-3xl font-bold">Salva Vidas</span>.
+        E uma menção especial ao nosso Prefeito <span className="xl:text-2xl font-bold">Keko do Armazém</span> pelo incentivo aos esportes, que vem fazendo com que os atletas de todas as modalidades do municipio se dediquem e saibam que o esporte <span className="xl:text-2xl font-bold">Salva Vidas</span>.
       </p>
 
       <p className={`text-center xl:text-8xl tracking-widest leading-loose lg:pt-12 font-allura  animate-wiggle`}>
