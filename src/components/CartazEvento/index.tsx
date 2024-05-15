9eea20e0e4ddfe1c4248744de56cc2bf1a25a58f
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-05-15 17:12:17 -0300

    Update: Cartaz

--- src/components/CartazEvento/index.tsx
+++ src/components/CartazEvento/index.tsx
@@ -9,7 +9,7 @@ export default function CartazEvento() {
         justify-center
         items-center
       `} id="cartazEvento">
-      <Image src={cartazEvento} width={428} height={428} alt="" className="py-4" />
+      <Image src={cartazEvento} width={428} height={428} alt="" className="pt-8" />
     </div>
   )
 }
